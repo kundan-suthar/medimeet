0d22ca77af9b97316cd56d0ec63795c7e1c36bd5
Kundan capFront <kundankumar.as@capfront.in>
2026-01-12 15:12:37 +0530

    add header with navigation and logo

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}`}>
         {/* header */}
+        <Header />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="py-32">
       Hello ....
       <br />
       <Button variant="default">Click me</Button>
