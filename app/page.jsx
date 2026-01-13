20f4a4b6b7ad01961c79e29865216b93cc92405f
Kundan Suthar <kundanss22@gmail.com>
2026-01-13 18:08:21 +0530

    add homepage features, success stories, cta card

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { features } from "@/lib/data";
-import { ArrowRight } from "lucide-react";
+import { creditBenefits, features, testimonials } from "@/lib/data";
+import { ArrowRight, Check, Stethoscope } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -47,7 +47,7 @@ export default function Home() {
                 </Button>
               </div>
             </div>
-            <div className="relative h-[400px] lg:h-[500px] rounded-xl overflow-hidden">
+            <div className="relative h-100 lg:lg:h-125 rounded-xl overflow-hidden">
               <Image
                 src="/banner2.png"
                 alt="Doctor consultant"
@@ -80,7 +80,9 @@ export default function Home() {
                     <div className="bg-emerald-900/20 p-3 rounded-lg w-fit mb-4">
                       {feature.icon}
                     </div>
-                    <CardTitle className="text-xl font-semibold text-white">{feature.title}</CardTitle>
+                    <CardTitle className="text-xl font-semibold text-white">
+                      {feature.title}
+                    </CardTitle>
                   </CardHeader>
                   <CardContent>
                     <p>{feature.description}</p>
@@ -89,6 +91,140 @@ export default function Home() {
               );
             })}
           </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-muted/30">
+        <div>
+          <div className="text-center mb-16">
+            <Badge
+              variant="outline"
+              className="bg-emerald-900/30 border-emerald-700/30 px-4 py-2 text-emerald-400 text-sm"
+            >
+              Affordable Healthcare
+            </Badge>
+            <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">
+              Consultaion Packages
+            </h2>
+            <p className="text-muted-foreground text-lg max-w-2xl mx-auto">
+              Choose the perfect consultation packages that fits your healthcare
+              needs.
+            </p>
+          </div>
+          {/* pricing table */}
+          <div>
+            <Card className="mt-12 bg-muted/20 border-emerald-900/30">
+              <CardHeader className="pb-2">
+                <CardTitle className="text-xl font-semibold text-white flex items-center">
+                  <Stethoscope className="h-5 w-5 mr-2 text-emerald-400 " />
+                  How are Credit system works
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <ul className="space-y-3">
+                  {creditBenefits.map((benefit, index) => {
+                    return (
+                      <li key={index} className="flex items-start">
+                        <div className="mr-3 mt-1 bg-emerald-900/20 p-1 rounded-full flex items-center ">
+                          <Check className="h-4 w-4 text-emerald-400" />{" "}
+                        </div>
+                        <p
+                          className="text-muted-foreground"
+                          dangerouslySetInnerHTML={{ __html: benefit }}
+                        />
+                      </li>
+                    );
+                  })}
+                </ul>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-muted/30">
+        <div>
+          <div className="text-center mb-16">
+            <Badge
+              variant="outline"
+              className="bg-emerald-900/30 border-emerald-700/30 px-4 py-2 text-emerald-400 text-sm"
+            >
+              Success Stories
+            </Badge>
+            <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">
+              What Our Users Say
+            </h2>
+            <p className="text-muted-foreground text-lg max-w-2xl mx-auto">
+              Hear from doctors and patients who use our platform.
+            </p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {testimonials.map((testimonial, index) => {
+              return (
+                <Card
+                  key={index}
+                  className="bg-emerald-900/20 hover:border-emerald-800/40 transition-all duration-300"
+                >
+                  <CardContent className="pt-6">
+                    <div className="flex items-center mb-4">
+                      <div className="h-12 w-12 rounded-full bg-emerald-900/20 flex items-center justify-center mr-4">
+                        <span className="text-emerald-400 font-bold">
+                          {testimonial.initials}
+                        </span>
+                      </div>
+                      <div>
+                        <h4 className="font-semibold text-white">
+                          {testimonial.name}
+                        </h4>
+                        <p className="text-muted-foreground">
+                          {testimonial.role}
+                        </p>
+                      </div>
+                    </div>
+                    <p className="text-muted-foreground">
+                      &quot;{testimonial.quote}&quot;
+                    </p>
+                  </CardContent>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 ">
+        <div className="container mx-auto px-4">
+          <Card className="bg-linear-to-r from-emerald-900/30 to-emerald-900/20 border-emerald-900/20">
+            <CardContent className="pt-8 md:p-12 lg:p-16 relative overflow-hidden">
+              <div>
+                <h2 className="text-3xl md:text-4xl font-bold text-white mb-6">
+                  Ready to take control of your healthcare.
+                </h2>
+                <p className="text-lg text-muted-foreground mb-8">
+                  Join thousands of users who have simplified their healthcare
+                  journey with our platform. Get started today and experience
+                  healthcare the way it should be.
+                </p>
+                <div className="flex flex-col sm:flex-row gap-4">
+                  <Button
+                    asChild
+                    size="lg"
+                    className="bg-emerald-600 text-white hover:bg-emerald-700"
+                  >
+                    <Link href="/sign-up">Sign Up</Link>
+                  </Button>
+                  <Button
+                    asChild
+                    variant="outline"
+                    size="lg"
+                    className="bg-emerald-600 text-white hover:bg-emerald-700"
+                  >
+                    <Link href="/pricing">Pricing</Link>
+                  </Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
         </div>
       </section>
     </div>
